3d725c90d22c73ef50572d2d58bb542fd0dde241
Andrew Grieve <agrieve@chromium.org>
2013-07-10 09:31:32 -0400

    [CB-4038] - Remove o.a.c.api namespace

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -18,10 +18,10 @@
 */
 package org.apache.cordova.core;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaInterface;
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
