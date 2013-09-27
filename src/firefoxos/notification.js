81ee294dae47142a087afadf6634f2820a860cd1
Piotr Zalewa <piotr@zalewa.info>
2013-09-27 14:33:08 +0200

    prompt added

--- src/firefoxos/notification.js
+++ src/firefoxos/notification.js
@@ -3,7 +3,7 @@ var firefoxos = require('cordova/platform');
 
 function _empty() {}
 
-function modal(message, callback, title, buttonLabels) {
+function modal(message, callback, title, buttonLabels, domObjects) {
     // create a modal window
     var box = document.createElement('div');
     box.classList.add('fxos-modal-window');
@@ -17,6 +17,10 @@ function modal(message, callback, title, buttonLabels) {
     var boxMessage = document.createElement('p');
     boxMessage.appendChild(document.createTextNode(message));
     box.appendChild(boxMessage);
+    // inject what's needed
+    if (domObjects) {
+        box.appendChild(domObjects);
+    }
     // add buttons and assign callbackButton on click
     for (var index = 0; index < buttonLabels.length; index++) {
         // TODO: first button is the default one (listens to Enter
@@ -53,6 +57,23 @@ var Notification = {
     confirm: function(successCallback, errorCallback, message, title, buttonLabels) {
         var _callback = (successCallback || _empty);
         modal(message, _callback, title, buttonLabels);
+    },
+    prompt: function(successCallback, errorCallback, message, title, buttonLabels, defaultText) {
+        var _tempcallback = (successCallback || _empty);
+        function _callback(labelIndex) {
+            var content = document.getElementById('prompt-input').value;
+            successCallback(labelIndex, content);
+        }
+        var inputParagraph = document.createElement('p');
+        inputParagraph.classList.add('input');
+        var inputElement = document.createElement('input');
+        inputElement.setAttribute('type', 'text');
+        inputElement.id = 'prompt-input';
+        if (defaultText) {
+            inputElement.setAttribute('placeholder', defaultText);
+        }
+        inputParagraph.appendChild(inputElement);
+        modal(message, _callback, title, buttonLabels, inputParagraph);
     }
 };
 
